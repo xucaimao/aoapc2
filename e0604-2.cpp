acd655e3ec6efed0e476b54a43e8b7cf5cfa8d4e
xucaimao <xcmgj@163.com>
2017-10-26 03:01:08 +0800

    edit e0604

--- e0604-2.cpp
+++ e0604-2.cpp
@@ -25,7 +25,9 @@ bool isOnBoard(int row,int col){
 void dfsAll(int sr,int sc,int step){
 	if(sr==tr && sc==tc){
 		//到目的地,打印一种解
-		
+		for(int i=0;i<=stop;i++)
+			printf("%c%c ",ans[i][0],ans[i][1]);
+		printf("\n");
 		return;
 	}
 	//超出棋盘范围
@@ -37,46 +39,31 @@ void dfsAll(int sr,int sc,int step){
 		//走一步棋,此处注意不能改变函数入口的sr,sc的值
 		int nsr=sr+dir[i][0];
 		int nsc=sc+dir[i][1];
-		solve(nsr,nsc,step+1);
+		stop++;
+		ans[stop][0]=nsr+'a';
+		ans[stop][1]=nsr+'1';
+		dfsAll(nsr,nsc,step+1);
+		stop--;
 		//因为前面没有改变值，这里就不需要做恢复数据的处理
 		}
 }
 
-//其实是一个dfs的过程
-void solve(int sr,int sc,int step){
-	if(sr==tr && sc==tc){
-		//到目的地
-		if(step<minstep)minstep=step;
-		return;
-	}
-	//超出棋盘范围
-	if(!isOnBoard(sr,sc)) 
-		return;		//这里其实就是一个剪枝
-	//步数超出既有解，不接着往下计算
-	if(step>minstep) 
-		return;		//这里其实就是一个剪枝
-	for(int i=0;i<8;i++){
-		//走一步棋,此处注意不能改变函数入口的sr,sc的值
-		int nsr=sr+dir[i][0];
-		int nsc=sc+dir[i][1];
-		solve(nsr,nsc,step+1);
-		//因为前面没有改变值，这里就不需要做恢复数据的处理
-		}
-}
 
 int main(){
 	freopen("e0604-2.in","r",stdin);
 	while( scanf("%s%s",ss,ts)==2 ){
-		//minstep=100000;
-		//printf("Input pos is : %s and %s !!\n",ss,ts);
+		printf("Input pos is : %s and %s !!\n",ss,ts);
+		stop=0;
 		int sr,sc;	//初始和目标位置
 		sc=ss[0]-'a';
 		sr=ss[1]-'1';
 		tc=ts[0]-'a';
 		tr=ts[1]-'1';
-		//printf("Input pos is : r%d,c%d and r%d,c%d !!\n",sr,sc,tr,tc);
-		solve(sr,sc,0);
-		printf("To get from %s to %s takes %d knight moves.\n",ss,ts,minstep);
+		ans[0][0]=ss[0];
+		ans[0][1]=ss[1];
+		printf("The path is : \n");
+		dfsAll(sr,sc,0);
+		
 	}
 	return 0;
 }
